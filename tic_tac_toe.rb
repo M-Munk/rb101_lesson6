ff38de3f38930d2302ce102732911a0e652649c9
Melanie Munk <mmunk1994@gmail.com>
2020-10-19 20:12:37 -0600

    refactor player choice

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -60,18 +60,34 @@ def initialize_board
   new_board
 end
 
+# def player_places_piece!(brd)
+#   square = ''
+#   loop do
+#     prompt "Choose a square (#{joinor(empty_squares(brd))}): "
+#     square = gets.chomp.to_i
+#     if empty_squares(brd).include?(square)
+#       break
+#     else
+#       prompt "Sorry, that's not a valid choice."
+#     end
+#   end
+#   brd[square] = PLAYER_MARKER
+# end
+
+def valid_space?(choice, brd)
+  valid_choice = %w(1 2 3 4 5 6 7 8 9)
+  valid_choice.include?(choice) && empty_squares(brd).include?(choice.to_i)
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
     prompt "Choose a square (#{joinor(empty_squares(brd))}): "
-    square = gets.chomp.to_i
-    if empty_squares(brd).include?(square)
-      break
-    else
-      prompt "Sorry, that's not a valid choice."
-    end
+    square = gets.chomp
+    break if valid_space?(square, brd)
+    prompt "Sorry, that's not a valid choice."
   end
-  brd[square] = PLAYER_MARKER
+  brd[square.to_i] = PLAYER_MARKER
 end
 
 def computer_choose_move(brd, marker)
