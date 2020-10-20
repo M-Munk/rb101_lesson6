2233f282ded269e3e528607786897da94c6d1085
Melanie Munk <mmunk1994@gmail.com>
2020-10-19 20:21:42 -0600

    cleanup

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -60,20 +60,6 @@ def initialize_board
   new_board
 end
 
-# def player_places_piece!(brd)
-#   square = ''
-#   loop do
-#     prompt "Choose a square (#{joinor(empty_squares(brd))}): "
-#     square = gets.chomp.to_i
-#     if empty_squares(brd).include?(square)
-#       break
-#     else
-#       prompt "Sorry, that's not a valid choice."
-#     end
-#   end
-#   brd[square] = PLAYER_MARKER
-# end
-
 def valid_space?(choice, brd)
   valid_choice = %w(1 2 3 4 5 6 7 8 9)
   valid_choice.include?(choice) && empty_squares(brd).include?(choice.to_i)
